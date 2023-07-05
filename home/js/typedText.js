bdbd2b601df6e8347d83862354245641a0e9a53a
Jacob Kulberg <76859773+JacobKulberg@users.noreply.github.com>
2023-07-04 21:10:33 -0400

    Sped up load animation on click and touch

--- home/js/typedText.js
+++ home/js/typedText.js
@@ -5,72 +5,142 @@ window.addEventListener('load', function () {
 	[...typedTexts].forEach((typedText) => {
 		let text = typedText.getElementsByClassName('typed-text')[0];
 		let cursor = typedText.getElementsByClassName('typed-text-cursor')[0];
+		let unfinished = typedText.getElementsByClassName('unfinished')[0];
 
-		setTimeout(() => {
-			cursor.style.height = '';
-			setTimeout(() => {
-				cursor.style.animation = 'blink 600ms step-end infinite';
-			}, 400);
-		}, 1000);
+		let mostRecentTimeoutId = NaN;
+		let mostRecentTimeout = null;
 
-		setTimeout(() => {
-			cursor.style.animation = 'none';
+		let spedUp = false;
 
-			let value = text.getAttribute('text-value');
-			let index = 0;
+		mostRecentTimeoutId = setTimeout(
+			(mostRecentTimeout = () => {
+				cursor.style.height = '';
+				mostRecentTimeoutId = setTimeout(
+					(mostRecentTimeout = () => {
+						cursor.style.animation = `blink ${spedUp ? 60 : 600}ms step-end infinite`;
 
-			function typeLetter() {
-				if (index < value.length) {
-					text.innerHTML += value[index];
-					cursor.style.left = `calc(51% + ${text.offsetWidth / 2}px)`;
-					index++;
-					setTimeout(typeLetter, 100);
-				} else {
-					window.addEventListener(
-						'resize',
-						(cursorResize = () => {
-							cursor.style.left = `calc(51% + ${text.offsetWidth / 2}px)`;
-						})
-					);
+						mostRecentTimeoutId = setTimeout(
+							(mostRecentTimeout = () => {
+								cursor.style.animation = 'none';
 
-					cursor.style.animation = 'blink 600ms step-end infinite';
-					setTimeout(() => {
-						cursor.style.height = '0px';
-						cursor.style.animation = 'none';
-						setTimeout(() => {
-							window.removeEventListener('resize', cursorResize);
-							cursor.parentNode.removeChild(cursor);
+								let value = text.getAttribute('text-value');
+								let index = 0;
 
-							let scale = window.innerWidth > window.innerHeight ? 0.45 : 0.75;
-							text.style.transformOrigin = 'top center';
-							text.style.transform = `translateX(-50%) scale(${scale})`;
-							text.style.top = `0px`;
+								function typeLetter() {
+									if (index < value.length) {
+										text.innerHTML += value[index];
+										cursor.style.left = `calc(51% + ${text.offsetWidth / 2}px)`;
+										index++;
+										mostRecentTimeoutId = setTimeout((mostRecentTimeout = typeLetter), 100);
+									} else {
+										window.addEventListener(
+											'resize',
+											(cursorResize = () => {
+												cursor.style.left = `calc(51% + ${text.offsetWidth / 2}px)`;
+											})
+										);
 
-							setTimeout(() => {
-								typedText.style.backgroundColor = 'transparent';
-								text.style.transition = 'unset';
+										cursor.style.animation = `blink ${spedUp ? 60 : 600}ms step-end infinite`;
+										mostRecentTimeoutId = setTimeout(
+											(mostRecentTimeout = () => {
+												cursor.style.height = '0px';
+												cursor.style.animation = 'none';
+												mostRecentTimeoutId = setTimeout(
+													(mostRecentTimeout = () => {
+														window.removeEventListener('resize', cursorResize);
+														cursor.parentNode.removeChild(cursor);
 
-								let textBounds = text.getBoundingClientRect();
-								spacer.style.height = `${textBounds.y + textBounds.height}px`;
+														let scale = window.innerWidth > window.innerHeight ? 0.45 : 0.75;
+														text.style.transformOrigin = 'top center';
+														text.style.transform = `translateX(-50%) scale(${scale})`;
+														text.style.top = `0px`;
 
-								setTimeout(() => {
-									document.querySelector('header hr').style.transform = 'scaleX(1)';
-								}, 200);
-							}, 800);
+														mostRecentTimeoutId = setTimeout(
+															(mostRecentTimeout = () => {
+																typedText.style.backgroundColor = 'transparent';
+																text.style.transition = 'unset';
 
-							window.addEventListener('resize', () => {
-								let scale = window.innerWidth > window.innerHeight ? 0.45 : 0.75;
-								text.style.transform = `translateX(-50%) scale(${scale})`;
+																let textBounds = text.getBoundingClientRect();
+																spacer.style.height = `${textBounds.y + textBounds.height}px`;
 
-								let textBounds = text.getBoundingClientRect();
-								spacer.style.height = `${textBounds.y + textBounds.height}px`;
-							});
-						}, 600);
-					}, 800);
-				}
-			}
+																mostRecentTimeoutId = setTimeout(
+																	(mostRecentTimeout = () => {
+																		document.querySelector('header hr').style.transform = 'scaleX(1)';
 
-			typeLetter();
-		}, 2800);
+																		mostRecentTimeoutId = setTimeout(
+																			(mostRecentTimeout = () => {
+																				if (unfinished.parentNode) unfinished.parentNode.removeChild(unfinished);
+
+																				decelerateTimeouts();
+																			}),
+																			300
+																		);
+																	}),
+																	200
+																);
+															}),
+															800
+														);
+
+														window.addEventListener('resize', () => {
+															let scale = window.innerWidth > window.innerHeight ? 0.45 : 0.75;
+															text.style.transform = `translateX(-50%) scale(${scale})`;
+
+															let textBounds = text.getBoundingClientRect();
+															spacer.style.height = `${textBounds.y + textBounds.height}px`;
+														});
+													}),
+													600
+												);
+											}),
+											800
+										);
+									}
+								}
+
+								typeLetter();
+							}),
+							1400
+						);
+					}),
+					400
+				);
+			}),
+			1000
+		);
+
+		unfinished.addEventListener('click', () => {
+			unfinished.parentNode.removeChild(unfinished);
+			accelerateTimeouts();
+		});
+		unfinished.addEventListener('touchstart', () => {
+			unfinished.parentNode.removeChild(unfinished);
+			accelerateTimeouts();
+		});
+
+		let originalSetTimeout = setTimeout;
+		function accelerateTimeouts() {
+			spedUp = true;
+
+			typedText.style.transition = 'transition: background-color 80ms ease-in-out';
+			text.style.transition = 'top 80ms ease-in-out, transform 80ms ease-in-out';
+			cursor.style.transition = 'height 25ms cubic-bezier(0.4, 0, 0.2, 1)';
+
+			window.setTimeout = function (callback, delay, ...args) {
+				return originalSetTimeout(callback, delay / 10, ...args);
+			};
+
+			clearTimeout(mostRecentTimeoutId);
+			mostRecentTimeout();
+		}
+
+		function decelerateTimeouts() {
+			spedUp = false;
+
+			typedText.style.transition = 'unset';
+			text.style.transition = 'unset';
+
+			window.setTimeout = originalSetTimeout;
+		}
 	});
 });
