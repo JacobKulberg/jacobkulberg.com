370f82b096608490bfc5bebf1795cf08326ba320
Jacob Kulberg <76859773+JacobKulberg@users.noreply.github.com>
2023-07-07 15:45:38 -0400

    Made pages for each link

--- home/js/main.js
+++ home/js/main.js
@@ -1,25 +1,33 @@
 window.addEventListener('load', () => {
-	let underline = document.querySelector('#selectedPage');
 	let links = document.querySelectorAll('header nav a');
+	let pages = document.getElementsByClassName('page');
+
+	let underline = document.querySelector('#selectedPage');
+
 	let selected = window.location.hash || '#home';
 
-	updateUnderline(document.querySelector(`header nav a[href="${selected}"]`));
+	selectPage(document.querySelector(`header nav a[href="${selected}"]`));
 
 	[...links].forEach((link) => {
 		link.addEventListener('click', () => {
-			updateUnderline(link);
+			selectPage(link);
 			selected = link.getAttribute('href');
 		});
 	});
 
 	window.addEventListener('resize', () => {
 		underline.style.transition = 'none';
-		updateUnderline(document.querySelector(`header nav a[href="${selected}"]`));
+		selectPage(document.querySelector(`header nav a[href="${selected}"]`));
 		underline.style.transition = '';
 	});
 
-	function updateUnderline(l) {
-		underline.style.left = `${l.offsetLeft}px`;
-		underline.style.width = `${l.offsetWidth}px`;
+	function selectPage(link) {
+		underline.style.left = `${link.offsetLeft}px`;
+		underline.style.width = `${link.offsetWidth}px`;
+
+		[...pages].forEach((page) => {
+			page.style.display = 'none';
+		});
+		document.getElementById(link.getAttribute('href').substring(1)).style.display = '';
 	}
 });
