f602058b569d322d68f35105955cb25b697599e1
Jacob Kulberg <76859773+JacobKulberg@users.noreply.github.com>
2023-07-08 23:47:11 -0400

    Reformatted typed text code

--- home/js/typedText.js
+++ home/js/typedText.js
@@ -1,26 +1,24 @@
-$(window).on('load', function() {
+$(window).on('load', () => {
 	let spacer = $('#header-spacer');
 
-	$('.typed-text-container').each(function() {
-		let typedText = $(this);
-		let text = typedText.find('.typed-text:first');
-		let cursor = typedText.find('.typed-text-cursor:first');
-		let unfinished = typedText.find('.unfinished:first');
+	let typedTexts = $('.typed-text-container');
+	typedTexts.each((index, typedText) => {
+		let text = $('.typed-text').first();
+		let cursor = $('.typed-text-cursor').first();
+		let unfinished = $('.unfinished').first();
 
 		let mostRecentTimeout = null;
 
 		let spedUp = false;
 
-		let mostRecentTimeoutId = setTimeout(mostRecentTimeout = function() {
-			cursor.css('height', '');
-			mostRecentTimeoutId = setTimeout(mostRecentTimeout = function() {
-				cursor.css({
-					'transition': 'none',
-					'animation': `blink ${spedUp ? 600 / 7 : 600}ms step-end infinite`
-				});
+		let mostRecentTimeoutId = setTimeout(mostRecentTimeout = () => {
+			$(cursor).css('height', '');
+			mostRecentTimeoutId = setTimeout(mostRecentTimeout = () => {
+				$(cursor).css('transition', 'none');
+				$(cursor).css('animation', `blink ${spedUp ? 600 / 7 : 600}ms step-end infinite`);
 
-				mostRecentTimeoutId = setTimeout(mostRecentTimeout = function() {
-					cursor.css('animation', 'none');
+				mostRecentTimeoutId = setTimeout(mostRecentTimeout = () => {
+					$(cursor).css('animation', 'none');
 
 					let value = text.attr('text-value');
 					let index = 0;
@@ -28,58 +26,60 @@ $(window).on('load', function() {
 					function typeLetter() {
 						if (index < value.length) {
 							text.html(text.html() + value[index]);
-							cursor.css('left', `calc(51% + ${text.width() / 2}px)`);
+							$(cursor).css('left', `calc(51% + ${$(text).width() / 2}px)`);
 							index++;
 							mostRecentTimeoutId = setTimeout(mostRecentTimeout = typeLetter, 100);
 						} else {
-							let cursorResize = function() {
-								cursor.css('left', `calc(51% + ${text.width() / 2}px)`);
-							}
+							window.addEventListener('resize', (cursorResize = () => {
+								$(cursor).css('left', `calc(51% + ${$(text).width() / 2}px)`);
+							}));
 
-							$(window).on('resize', cursorResize);
-
-							cursor.css({
-								'animation': `blink ${spedUp ? 600 / 7 : 600}ms step-end infinite`,
-								'transition': '',
-								'height': '0'
-							});
-
-							mostRecentTimeoutId = setTimeout(mostRecentTimeout = function() {
-								$(window).off('resize', cursorResize);
-								cursor.remove();
-
-								let scale = $(window).innerWidth() > $(window).innerHeight() ? 0.45 : 0.75;
-								text.css({
-									'transformOrigin': 'top center',
-									'transform': `translateX(-50%) scale(${scale})`,
-									'top': '0'
+							$(cursor).css('animation', `blink ${spedUp ? 600 / 7 : 600}ms step-end infinite`);
+							mostRecentTimeoutId = setTimeout(mostRecentTimeout = () => {
+								$(cursor).css({
+									transition: '',
+									height: '0',
+									animation: 'none'
 								});
 
-								mostRecentTimeoutId = setTimeout(mostRecentTimeout = function() {
-									typedText.css('backgroundColor', 'transparent');
-									text.css('transition', 'none');
+								mostRecentTimeoutId = setTimeout(mostRecentTimeout = () => {
+									$(window).off('resize', cursorResize);
+									cursor.remove();
 
-									let textBounds = text[0].getBoundingClientRect();
-									spacer.css('height', `${textBounds.height}px`);
-
-									mostRecentTimeoutId = setTimeout(mostRecentTimeout = function() {
-										$('header hr:first').css('transform', 'scaleX(1)');
-
-										mostRecentTimeoutId = setTimeout(mostRecentTimeout = function() {
-											if (unfinished.parent().length) unfinished.remove();
-
-											decelerateTimeouts();
-										}, 300);
-									}, 200);
-								}, 850);
-
-								$(window).on('resize', function() {
 									let scale = $(window).innerWidth() > $(window).innerHeight() ? 0.45 : 0.75;
-									text.css('transform', `translateX(-50%) scale(${scale})`);
+									$(text).css({
+										transformOrigin: 'top center',
+										transform: `translateX(-50%) scale(${scale})`,
+										top: `0`
+									})
 
-									spacer.css('height', `${text[0].getBoundingClientRect().height}px`);
-								});
-							}, 600);
+									mostRecentTimeoutId = setTimeout(mostRecentTimeout = () => {
+										$(typedText).css('backgroundColor', 'transparent');
+										$(text).css('transition', 'none');
+
+										$(spacer).css('height', `${text[0].getBoundingClientRect().height}px`);
+
+										mostRecentTimeoutId = setTimeout(mostRecentTimeout = () => {
+											$('header hr').css('transform', 'scaleX(1)');
+
+											mostRecentTimeoutId = setTimeout(mostRecentTimeout = () => {
+												if (unfinished.parent()) {
+													unfinished.remove();
+												}
+
+												decelerateTimeouts();
+											}, 300);
+										}, 200);
+									}, 850);
+
+									$(window).on('resize', () => {
+										let scale = $(window).innerWidth() > $(window).innerHeight() ? 0.45 : 0.75;
+										$(text).css('transform', `translateX(-50%) scale(${scale})`);
+
+										$(spacer).css('height', `${text[0].getBoundingClientRect().height}px`);
+									});
+								}, 600);
+							}, 800);
 						}
 					}
 
@@ -88,8 +88,8 @@ $(window).on('load', function() {
 			}, 400);
 		}, 1000);
 
-		unfinished.on('click touchstart', function() {
-			$(this).remove();
+		$(unfinished).on('click touchstart', () => {
+			unfinished.remove();
 			accelerateTimeouts();
 		});
 
@@ -97,10 +97,10 @@ $(window).on('load', function() {
 		function accelerateTimeouts() {
 			spedUp = true;
 
-			typedText.css('transition', `background-color ${800 / 7}ms ease-in-out`);
-			text.css('transition', `top ${800 / 7}ms ease-in-out, transform ${800 / 7}ms ease-in-out`);
+			$(typedText).css('transition', `background-color ${800 / 7}ms ease-in-out`);
+			$(text).css('transition', `top ${800 / 7}ms ease-in-out, transform ${800 / 7}ms ease-in-out`)
 
-			window.setTimeout = function(callback, delay, ...args) {
+			window.setTimeout = function (callback, delay, ...args) {
 				return originalSetTimeout(callback, delay / 7, ...args);
 			};
 
@@ -111,8 +111,8 @@ $(window).on('load', function() {
 		function decelerateTimeouts() {
 			spedUp = false;
 
-			typedText.css('transition', 'none');
-			text.css('transition', 'none');
+			$(typedText).css('transition', 'none');
+			$(text).css('transition', 'none');
 
 			window.setTimeout = originalSetTimeout;
 		}
