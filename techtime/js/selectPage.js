c3b1f7431a86c2eea0973cdc4f2cd21788603916
Jacob Kulberg <76859773+JacobKulberg@users.noreply.github.com>
2023-07-23 09:24:26 -0400

    Added background color fade when clicking on a techtime section

--- techtime/js/selectPage.js
+++ techtime/js/selectPage.js
@@ -23,8 +23,11 @@ $(window).on('load', () => {
 	});
 
 	$(window).on('resize orientationchange', () => {
-		underline.css('transition', 'none');
-		selectPage($(`header nav a[href="${selected}"]`));
+		underline.css({
+			transition: 'none',
+			left: `${$(`header nav a[href="${selected}"]`).offset().left}px`,
+			width: `${$(`header nav a[href="${selected}"]`).width()}px`,
+		});
 		setTimeout(() => {
 			underline.css('transition', '');
 		}, 0);
@@ -36,9 +39,12 @@ $(window).on('load', () => {
 			width: `${link.width()}px`,
 		});
 
-		$(`#${link.attr('href').slice(1)}`).css({
-			opacity: '',
-			pointerEvents: '',
+		links.each((index, link) => {
+			$($(link).attr('href')).css('backgroundColor', '');
 		});
+		$(link.attr('href')).css('backgroundColor', 'rgba(255, 255, 50, 0.25)');
+		setTimeout(() => {
+			$(link.attr('href')).css('backgroundColor', '');
+		}, 1000);
 	}
 });
