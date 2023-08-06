f18b169074aa542b911b94cb8eb5f283c633b9ff
Jacob Kulberg <76859773+JacobKulberg@users.noreply.github.com>
2023-08-06 10:02:33 -0400

    Added loading animation to allow full page to load

--- projects/banana-bonanza/js/main.js
+++ projects/banana-bonanza/js/main.js
@@ -3,3 +3,15 @@ const $window = $(window);
 $(document).on('dblclick', function (e) {
 	e.preventDefault();
 });
+
+$window.on('load', () => {
+	$('#loading img').on('animationiteration', () => {
+		$('#loading img').css('animation', 'none');
+
+		$('#loading').css('opacity', '0');
+
+		setTimeout(() => {
+			$('#loading').css('pointer-events', 'none');
+		}, 250);
+	});
+});
