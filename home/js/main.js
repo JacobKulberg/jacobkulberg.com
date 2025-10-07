6f2cd4211388267fe9d2488eda404632b2cb8fb6
JACOB-COMPUTER\jacob <jacobkulberg@gmail.com>
2025-10-06 22:26:07 -0400

    fix Contact Me overlapping Experience

--- home/js/main.js
+++ home/js/main.js
@@ -69,7 +69,7 @@ $(document).ready(function () {
 
 	$('#experience').css('height', $('.experience-container .work').height() + $('#experience > h2').outerHeight() + $('#experience > .work-education-toggle').outerHeight() + $('#contact-me > h2').outerHeight() + 48 + 'px');
 
-	$(window).on('resize orientationchange', () => {
+	$(window).on('resize orientationchange load', () => {
 		if ($workButton.hasClass('active')) {
 			$('#experience').css('height', $('.experience-container .work').height() + $('#experience > h2').outerHeight() + $('#experience > .work-education-toggle').outerHeight() + $('#contact-me > h2').outerHeight() + 48 + 'px');
 		} else {
